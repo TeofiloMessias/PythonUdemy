41262779abf04a19537d7f8bca226b1444787cb7
Teofilo Messias Filho <re045002@qintess.com>
2023-09-19 09:22:18 -0300

    Iniciando a seção 22 - Checagem de tipos em python

---
+++ Secao22-Checagem_de_tipos_em_python/aula153_o_que_vamos_aprender_nesta_secao.py
@@ -0,0 +1,10 @@
+"""
+- Tipagem de dados Dinâmica x Estática
+- Duck Typing
+- Type Hinting
+- Checagem de Tipos com MyPy
+- Prós e Contras do uso de Type Hiting
+- Fazendo uso de Annotations
+- Tipos em Comentários
+- Tipos em Python na Prática
+"""
